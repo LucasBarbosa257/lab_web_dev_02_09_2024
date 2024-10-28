d824486f203a8b378faa7d77dd96051f3ea6d7b6
Lucas Barbosa <lucasbdev257@gmail.com>
2024-10-28 16:32:24 -0300

    To-Do List

--- 21_10_2024/script.js
+++ 21_10_2024/script.js
@@ -1,10 +1,10 @@
 let text_area = document.getElementById("text-area");
 
-function atualizarContador() {
+function updateCounter() {
     document.getElementById("counter").innerText = text_area.value.length;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     text_area.value = text_area.value.trim();
-    atualizarContador();
+    updateCounter();
 });
